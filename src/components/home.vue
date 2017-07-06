524552c210aad3014ba39f301c6679ecd4691c8b
hoivee <xjhovee@qq.com>
2017-07-06 11:46:48 +0800

    weather-show组件开发

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <app-header :city-name="cityName"></app-header>
-    <weather-show></weather-show>
+    <weather-show
+      :weather="weather"
+      :tmp="tmp"
+      :last-tmp="lastTmp"
+      :aqi="aqi"
+    ></weather-show>
   </div>
 
 </template>
@@ -14,6 +19,10 @@
     data() {
       return {
         cityName: '城市',
+        weather: '阴',
+        tmp: 32,
+        lastTmp: '比昨天高5°C',
+        aqi: '123 轻度',
       };
     },
     methods: {
--- src/components/weather-show.vue
+++ src/components/weather-show.vue
@@ -1,17 +1,71 @@
 <template>
-  <div class="weather-show">
-    <div>
-      天气状况
-    </div>
+  <div class="weather-info">
+    <p class="weather-info-text">{{weather}}</p>
+    <p class="weather-info-tmp">{{tmp}}</p>
+    <p class="weather-info-lastTmp">{{lastTmp}}°</p>
+    <p class="weather-info-aqi"><i class="icon-aqi"></i> {{aqi}}</p>
   </div>
 </template>
 
 <style lang="stylus">
-.weather-show
-  padding: .5rem 0 0
+@require '../style/variable.styl'
+
+.weather-info
+  padding: $height-app-header + .7rem 0 1rem
   height: 10rem
+  text-align: center
+  color: #0b2b60
+.weather-info-text
+  font-size: .2rem
+  margin: 0 0 .35rem
+.weather-info-tmp
+  position: relative
+  font-size: 1.1rem
+  &:after
+    content: '°'
+    position: absolute
+.weather-info-lastTmp
+  font-size: .16rem
+  margin: .15rem 0
+.weather-info-aqi
+  display: inline-block
+  padding: .05rem .15rem
+  font-size: .18rem
+  border: solid 2px $color-base
+  border-radius: .2rem
+.icon-aqi
+  display: inline-block
+  width: .18rem
+  height: .18rem
+  background-image: url('../../static/icon-aqi.svg')
+  background-repeat: no-repeat
+  background-size: contain
+  background-position: center
+  vertical-align: middle
 </style>
 
 <script>
-
+export default {
+  data() {
+    return {
+    };
+  },
+  props: {
+    // 天气
+    weather: {
+      type: String,
+    },
+    // 体感温度
+    tmp: {
+      type: Number,
+    },
+    lastTmp: {
+      type: String,
+    },
+    // 空气质量指数
+    aqi: {
+      type: String,
+    },
+  },
+};
 </script>
